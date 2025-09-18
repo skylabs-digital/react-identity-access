f35a7c5ef59df4f97245801ca1a36204e507794a
Fernando Caamaño <guettomusick@gmail.com>
2025-09-18 00:13:28 -0300

    chore: migrate from react-router to react-router-dom dependency

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Navigate, useLocation } from 'react-router';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../providers/AuthProvider';
 import { UserType, Permission } from '../types/api';
 
--- src/components/TenantRoute.tsx
+++ src/components/TenantRoute.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Navigate, useLocation } from 'react-router';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useTenantInfo } from '../providers/TenantProvider';
 
 export interface TenantRouteProps {
